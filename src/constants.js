d92ec674f9edbd6e02da38c72a2593c8b008e714
1t1e1 <1t1ebir@gmail.com>
2020-05-16 02:56:45 +0300

    edit apiHost

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const categories = ["JavaScript", "HTML", "CSS", "Python", "JAVA"];
 export const ratings = [1, 2, 3, 4, 5];
 export const statuses = ["Not read", "Read"];
-export const apiHost = "https://5eb6ac52875f1a00167e0faf.mockapi.io";
+export const apiHost = "https://5ebf287d00eabe0016fa39aa.mockapi.io/";
+// export const apiHost = "https://5eb6ac52875f1a00167e0faf.mockapi.io";
