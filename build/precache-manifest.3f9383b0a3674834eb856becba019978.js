9a852f4d23a8a5026006c008e062e02360e57784
1t1e1 <1t1ebir@gmail.com>
2020-06-17 17:00:57 +0300

    edit for deployment

---
+++ build/precache-manifest.3f9383b0a3674834eb856becba019978.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09947a4d08d3d51c976c6f4e7b5051fe",
+    "url": "/bootcamp-bookshelf/index.html"
+  },
+  {
+    "revision": "9666cf09d1712402a1a0",
+    "url": "/bootcamp-bookshelf/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "832d921458a24431d541",
+    "url": "/bootcamp-bookshelf/static/css/main.814754d9.chunk.css"
+  },
+  {
+    "revision": "9666cf09d1712402a1a0",
+    "url": "/bootcamp-bookshelf/static/js/2.0440e461.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/bootcamp-bookshelf/static/js/2.0440e461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "832d921458a24431d541",
+    "url": "/bootcamp-bookshelf/static/js/main.18ca4c96.chunk.js"
+  },
+  {
+    "revision": "2bb8868db1d1d690948d",
+    "url": "/bootcamp-bookshelf/static/js/runtime-main.849c3f76.js"
+  }
+]);
